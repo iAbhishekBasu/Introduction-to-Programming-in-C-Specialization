9ac1aae885ceb175b5cdb330232f5590abcf0436
Coursera Learner <nobody@nowhere.nul>
2020-05-27 18:30:55 +0000

    changed

--- c2prj1_cards/cards.c
+++ c2prj1_cards/cards.c
@@ -5,154 +5,111 @@
 
 
 void assert_card_valid(card_t c) {
-  // this function check if the card passed has valid values
-  assert(c.value >= 2 && c.value <= VALUE_ACE);
-  assert(c.suit >= SPADES && c.suit <= CLUBS);
-
+  unsigned val = c.value;
+  suit_t suit = c.suit;
+  assert(2 <= val && val <= VALUE_ACE && SPADES <= suit && suit <= CLUBS);
 }
 
-const char * ranking_to_string(hand_ranking_t r) {
-  switch (r){
+
+const char *ranking_to_string(hand_ranking_t r) {
+  switch (r) {
   case STRAIGHT_FLUSH:
     return "STRAIGHT_FLUSH";
-    break;
   case FOUR_OF_A_KIND:
     return "FOUR_OF_A_KIND";
-    break;
   case FULL_HOUSE:
     return "FULL_HOUSE";
-    break;
   case FLUSH:
     return "FLUSH";
-    break;
   case STRAIGHT:
     return "STRAIGHT";
-    break;
   case THREE_OF_A_KIND:
     return "THREE_OF_A_KIND";
-    break;
   case TWO_PAIR:
     return "TWO_PAIR";
-    break;
   case PAIR:
     return "PAIR";
-    break;
-  case NOTHING:
-    return "NOTHING";
-    break;
   default:
-    return "INVALID";
-    break;
+    return "NOTHING";   // r == NOTHING
   }
 }
+
 
 char value_letter(card_t c) {
-  // Return the symbol that corresponds to the value of the card
-  if(c.value < 10)
-    return '0'+c.value;
-  else{
-    switch(c.value){
-    case 10:
-      return '0';
-      break;
-    case VALUE_ACE:
-      return 'A';
-      break;
-    case VALUE_KING:
-      return 'K';
-      break;
-    case VALUE_QUEEN:
-      return 'Q';
-      break;
-    case VALUE_JACK:
-      return 'J';
-      break;
-    }
+  int val = c.value;
+  if (val <= 9) {
+    return '0' + val;
+  } else if (val == 10) {
+    return '0';
+  } else if (val == VALUE_JACK){
+    return 'J';
+  } else if (val == VALUE_QUEEN) {
+    return 'Q';
+  } else if (val == VALUE_KING) {
+    return 'K';
   }
+  return 'A';   // val == VALUE_ACE
 }
 
+
 char suit_letter(card_t c) {
-  switch (c.suit){
-  case SPADES:
+  int suit = c.suit;
+  if (suit == SPADES) {
     return 's';
-    break;
-  case HEARTS:
+  } else if (suit == HEARTS) {
     return 'h';
-    break;
-  case DIAMONDS:
+  } else if (suit == DIAMONDS) {
     return 'd';
-    break;
-  case CLUBS:
-    return 'c';
-    break;
-  default:
-    return ' ';
-    break;
   }
+  return 'c';   // suit == CLUBS
 }
+
 
 void print_card(card_t c) {
   printf("%c%c", value_letter(c), suit_letter(c));
 }
 
+
 card_t card_from_letters(char value_let, char suit_let) {
-  int value;
+  unsigned value;
+  if ('1' <= value_let && value_let <= '9') {
+    value= value_let - '0';
+  } else if (value_let == '0') {
+    value= 10;
+  } else if (value_let == 'J') {
+    value = VALUE_JACK;
+  } else if (value_let == 'Q') {
+    value = VALUE_QUEEN;
+  } else if (value_let == 'K') {
+    value = VALUE_KING;
+  } else {   // value_let == 'A'
+    value = VALUE_ACE;
+  }
+
   suit_t suit;
-
-  // convert the characters into values
-  if (value_let > '0' && value_let < '0'+9)
-    value = value_let-'0';
-  else{
-    switch (value_let){
-    case '0':
-      value = 10;
-      break;
-    case 'A':
-      value = VALUE_ACE;
-      break;
-    case 'K':
-      value = VALUE_KING;
-      break;
-    case 'Q':
-      value = VALUE_QUEEN;
-      break;
-    case 'J':
-      value = VALUE_JACK;
-      break;
-    }
-  }
-  // convert characters into suits
-  switch(suit_let){
-  case 's':
+  if (suit_let == 's') {
     suit = SPADES;
-    break;
-  case 'h':
+  } else if (suit_let == 'h') {
     suit = HEARTS;
-    break;
-  case 'd':
+  } else if (suit_let == 'd') {
     suit = DIAMONDS;
-    break;
-  case'c':
+  } else {   // suit_let == 'c'
     suit = CLUBS;
-    break;
   }
 
-  card_t temp;
-  temp.value = value;
-  temp.suit = suit;
-  assert_card_valid(temp);
-  return temp;
+  card_t ans;
+  ans.value = value;
+  ans.suit = suit;
+  return ans;
 }
 
+
 card_t card_from_num(unsigned c) {
+  unsigned value = (c % 13) + 2;  // 0 <= c % 13 <= 12, 2 <= (c % 13) + 2 <= 14
+  suit_t suit = c / 13;
 
-  int suit_n = c/13;
-  int value_n = c%13+2;
-
-  card_t temp;
-  temp.value = value_n;
-  temp.suit = suit_n;
-  assert_card_valid(temp);
-
-  return temp;
+  card_t ans;
+  ans.value = value;
+  ans.suit = suit;
+  return ans;
 }
